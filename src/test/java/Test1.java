18fee5021b36265710fb2beb4c4913a23e3ef9e3
Mohit Malik <mohit1mali@gmail.com>
2024-08-14 23:59:29 +0530

    POM dependencies Appium driver Launch app

---
+++ src/test/java/Test1.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Test1{
+
+    //    AndroidDriver driver;
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        driver = new ChromeDriver();
+
+
+    }
+
+    @Test
+    public void Test() throws InterruptedException {
+        driver.get("https://www.google.com");
+        Thread.sleep(2000);
+    }
+
+    @AfterTest
+    public void close(){
+        driver.quit();
+    }
+}
